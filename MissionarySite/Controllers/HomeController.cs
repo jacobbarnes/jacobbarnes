ad0e7ba7f14efbe9c3534fafea340b331be74c1c
jacobbarnes <57917206+jacobbarnes@users.noreply.github.com>
2019-11-20 16:01:54 -0700

    deleted comment

--- MissionarySite/Controllers/HomeController.cs
+++ MissionarySite/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace MissionarySite.Controllers
 {
     public class HomeController : Controller
-    {//abc
+    {
         // GET: Home
         public ActionResult Index()
         {
